3526f1075bcdd6c4019f1d4da837f1058e106821
Enoc Leonrd <Leonrd@gmail.com>
2016-09-28 01:20:56 +0100

    fixed dotfile location bug

--- remote-setup.sh
+++ remote-setup.sh
@@ -7,7 +7,7 @@ if [ -z "$CMD" ]; then
   echo "No curl or wget available. Aborting."
 else
   echo "Installing dotfiles"
-  mkdir -p "$HOME/dotfiles" && \
+  mkdir -p "$HOME/.dotfiles" && \
   eval "$CMD https://github.com/micjagga/Dotfiles/tarball/master | tar -xzv -C ~/.dotfiles --strip-components=1 --exclude='{.gitignore}'"
-  . "$HOME/dotfiles/install.sh"
+  . "$HOME/.dotfiles/install.sh"
 fi
